7b6e164f65d36e9610ec12ffedc0cec899852861
Luis Coronel <luis.coronel@wizeline.com>
2021-03-18 15:17:12 -0600

    tweak for navbar tests

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,20 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import Navbar from './Navbar.component';
 
-it('test the search video option is available in the navbar', () => {
-  render(<Navbar />);
-  const searchVideoOption = screen.getByText(/Search video .../i);
-  expect(searchVideoOption).toBeInTheDocument();
-});
+describe('Navbar component tests', () => {
 
-it('test the light option is rendering in the navbar', () => {
-  render(<Navbar />);
-  const lightOption = screen.getByText(/Light/i);
-  expect(lightOption).toBeInTheDocument();
-});
+  beforeEach(() => {
+    render(<Navbar />);
+  });
 
-it('test the dark option is rendering in the navbar', () => {
-  render(<Navbar />);
-  const darkOption = screen.getByText(/Dark/i);
-  expect(darkOption).toBeInTheDocument();
+  it('test the search video option is available in the navbar', () => {
+    const searchVideoOption = screen.getByText(/Search video .../i);
+    expect(searchVideoOption).toBeInTheDocument();
+  });
+  
+  it('test the light option is rendering in the navbar', () => {
+    const lightOption = screen.getByText(/Light/i);
+    expect(lightOption).toBeInTheDocument();
+  });
+  
+  it('test the dark option is rendering in the navbar', () => {
+    const darkOption = screen.getByText(/Dark/i);
+    expect(darkOption).toBeInTheDocument();
+  });
 });
